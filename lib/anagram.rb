5952f1e86572d5a522e120d0090d0836978be944
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-05-11 13:58:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,4 +7,9 @@ class Anagram
   def initialize(word) 
     @word = word
   end 
+  
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("").sort
+ end 
 end
